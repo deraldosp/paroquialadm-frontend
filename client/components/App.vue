124454549512881372a22f28e309fb8955df6e5b
Deraldo Nascimento <deraldosp@gmail.com>
2021-06-08 00:12:02 -0300

    mock brandheader

--- client/components/App.vue
+++ client/components/App.vue
@@ -1,6 +1,32 @@
 <template>
   <div id="app">
     <div class="brandheader">
+      <div class="h-100 d-flex justify-content-between align-items-center">
+        
+        <div class="w-50 d-flex justify-content-center">
+          <b-input placeholder="Busca" style="border-radius: 20px"></b-input>
+        </div>
+
+
+        <div class="w-50 d-flex justify-content-end">  
+          <div class="h-100 d-flex align-items-center mr-3">
+            <b-avatar variant="info" class="mr-2"></b-avatar>
+            <div>Deraldo Nascimento</div>
+          </div>
+
+          <div class="btn btn-light rounded-circle mr-3">
+            <span class="material-icons-round">notifications_none</span>
+          </div>
+
+          <div class="btn btn-light rounded-circle mr-3">
+            <span class="material-icons">star</span>
+          </div>
+
+          <div class="btn btn-light rounded-circle">
+            <span class="material-icons">logout</span>
+          </div>
+        </div>
+      </div>
       
     </div>
     <div>
@@ -70,7 +96,7 @@ body {
 
 .btn-teste {
   position: fixed;
-  top: 0;
+  bottom: 0;
   right: 20px;
 }
 </style>
