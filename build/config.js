ddaec692a7453c9870ad07626ee92b047d572d50
Deraldo Nascimento <d.do.nascimento.neto@avanade.com>
2022-02-09 22:37:37 -0300

    Correção

--- build/config.js
+++ build/config.js
@@ -10,5 +10,5 @@ module.exports = {
   },
   // when you use electron please set to relative path like ./
   // otherwise only set to absolute path when you're using history mode
-  publicPath: process.env.NODE_ENV === 'production' ? '/paroquialadm-frontend' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/paroquialadm-frontend/' : '/'
 }
